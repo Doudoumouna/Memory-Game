3d4ecae97bb0aba2c127301f20a6864da8bac645
Doudoumouna <eboalucien@yahoo.it>
2023-06-13 17:06:11 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,403 @@
+const CardArray = [
+  {
+    name: 'fries',
+    img: 'images/fries.png',
+  },
+  {
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png',
+  },
+  {
+    name: 'hotdog',
+    img: 'images/hotdog.png',
+  },
+  {
+    name: 'ice-cream',
+    img: 'images/ice-cream.png',
+  },
+  {
+    name: 'milkshake',
+    img: 'images/milkshake.png',
+  },
+  {
+    name: 'pizza',
+    img: 'images/pizza.png',
+  },
+
+  {
+    name: 'fries',
+    img: 'images/fries.png',
+  },
+  {
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png',
+  },
+  {
+    name: 'hotdog',
+    img: 'images/hotdog.png',
+  },
+  {
+    name: 'ice-cream',
+    img: 'images/ice-cream.png',
+  },
+  {
+    name: 'milkshake',
+    img: 'images/milkshake.png',
+  },
+  {
+    name: 'pizza',
+    img: 'images/pizza.png',
+  },
+
+  {
+    name: 'fries',
+    img: 'images/fries.png',
+  },
+  {
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png',
+  },
+  {
+    name: 'hotdog',
+    img: 'images/hotdog.png',
+  },
+  {
+    name: 'ice-cream',
+    img: 'images/ice-cream.png',
+  },
+  {
+    name: 'milkshake',
+    img: 'images/milkshake.png',
+  },
+  {
+    name: 'pizza',
+    img: 'images/pizza.png',
+  },
+  {
+    name: 'fries',
+    img: 'images/fries.png',
+  },
+  {
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png',
+  },
+  {
+    name: 'hotdog',
+    img: 'images/hotdog.png',
+  },
+  {
+    name: 'ice-cream',
+    img: 'images/ice-cream.png',
+  },
+  {
+    name: 'milkshake',
+    img: 'images/milkshake.png',
+  },
+  {
+    name: 'pizza',
+    img: 'images/pizza.png',
+  },
+  {
+    name: 'fries',
+    img: 'images/fries.png',
+  },
+  {
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png',
+  },
+  {
+    name: 'hotdog',
+    img: 'images/hotdog.png',
+  },
+  {
+    name: 'ice-cream',
+    img: 'images/ice-cream.png',
+  },
+  {
+    name: 'milkshake',
+    img: 'images/milkshake.png',
+  },
+  {
+    name: 'pizza',
+    img: 'images/pizza.png',
+  },
+  {
+    name: 'fries',
+    img: 'images/fries.png',
+  },
+  {
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png',
+  },
+  {
+    name: 'hotdog',
+    img: 'images/hotdog.png',
+  },
+  {
+    name: 'ice-cream',
+    img: 'images/ice-cream.png',
+  },
+  {
+    name: 'milkshake',
+    img: 'images/milkshake.png',
+  },
+  {
+    name: 'pizza',
+    img: 'images/pizza.png',
+  },
+  {
+    name: 'fries',
+    img: 'images/fries.png',
+  },
+  {
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png',
+  },
+  {
+    name: 'hotdog',
+    img: 'images/hotdog.png',
+  },
+  {
+    name: 'ice-cream',
+    img: 'images/ice-cream.png',
+  },
+  {
+    name: 'milkshake',
+    img: 'images/milkshake.png',
+  },
+  {
+    name: 'pizza',
+    img: 'images/pizza.png',
+  },
+  {
+    name: 'fries',
+    img: 'images/fries.png',
+  },
+  {
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png',
+  },
+  {
+    name: 'hotdog',
+    img: 'images/hotdog.png',
+  },
+  {
+    name: 'ice-cream',
+    img: 'images/ice-cream.png',
+  },
+  {
+    name: 'milkshake',
+    img: 'images/milkshake.png',
+  },
+  {
+    name: 'pizza',
+    img: 'images/pizza.png',
+  },
+  {
+    name: 'fries',
+    img: 'images/fries.png',
+  },
+  {
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png',
+  },
+  {
+    name: 'hotdog',
+    img: 'images/hotdog.png',
+  },
+  {
+    name: 'ice-cream',
+    img: 'images/ice-cream.png',
+  },
+  {
+    name: 'milkshake',
+    img: 'images/milkshake.png',
+  },
+  {
+    name: 'pizza',
+    img: 'images/pizza.png',
+  },
+  {
+    name: 'fries',
+    img: 'images/fries.png',
+  },
+  {
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png',
+  },
+  {
+    name: 'hotdog',
+    img: 'images/hotdog.png',
+  },
+  {
+    name: 'ice-cream',
+    img: 'images/ice-cream.png',
+  },
+  {
+    name: 'milkshake',
+    img: 'images/milkshake.png',
+  },
+  {
+    name: 'pizza',
+    img: 'images/pizza.png',
+  },
+  {
+    name: 'fries',
+    img: 'images/fries.png',
+  },
+  {
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png',
+  },
+  {
+    name: 'hotdog',
+    img: 'images/hotdog.png',
+  },
+  {
+    name: 'ice-cream',
+    img: 'images/ice-cream.png',
+  },
+  {
+    name: 'milkshake',
+    img: 'images/milkshake.png',
+  },
+  {
+    name: 'pizza',
+    img: 'images/pizza.png',
+  },
+  {
+    name: 'fries',
+    img: 'images/fries.png',
+  },
+  {
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png',
+  },
+  {
+    name: 'hotdog',
+    img: 'images/hotdog.png',
+  },
+  {
+    name: 'ice-cream',
+    img: 'images/ice-cream.png',
+  },
+  {
+    name: 'milkshake',
+    img: 'images/milkshake.png',
+  },
+  {
+    name: 'pizza',
+    img: 'images/pizza.png',
+  },
+  {
+    name: 'fries',
+    img: 'images/fries.png',
+  },
+  {
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png',
+  },
+  {
+    name: 'hotdog',
+    img: 'images/hotdog.png',
+  },
+  {
+    name: 'ice-cream',
+    img: 'images/ice-cream.png',
+  },
+  {
+    name: 'milkshake',
+    img: 'images/milkshake.png',
+  },
+  {
+    name: 'pizza',
+    img: 'images/pizza.png',
+  },
+  {
+    name: 'fries',
+    img: 'images/fries.png',
+  },
+  {
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png',
+  },
+  {
+    name: 'hotdog',
+    img: 'images/hotdog.png',
+  },
+  {
+    name: 'ice-cream',
+    img: 'images/ice-cream.png',
+  },
+  {
+    name: 'milkshake',
+    img: 'images/milkshake.png',
+  },
+  {
+    name: 'pizza',
+    img: 'images/pizza.png',
+  },
+];
+
+CardArray.sort(() => 0.5 - Math.random());
+
+const gridDisplay = document.querySelector('#grid');
+const resultDisplay = document.querySelector('#result');
+let cardsChosen = [];
+let cardsChosenIds = [];
+const cardsWon = [];
+
+function createBoard() {
+  for (let i = 0; i < CardArray.length; i++) {
+    const card = document.createElement('img');
+    card.setAttribute('src', 'images/blank.png');
+    card.setAttribute('data-id', i);
+    card.addEventListener('click', flipCard);
+    gridDisplay.appendChild(card);
+  }
+}
+createBoard();
+
+function checkMatch() {
+  const cards = document.querySelectorAll('img');
+  const optionOneId = cardsChosenIds[0];
+  const optionIwoId = cardsChosenIds[1];
+  console.log(cards);
+  console.log('check for match!');
+  if (optionOneId == optionIwoId) {
+    cards[optionOneId].setAttribute('src', 'images / blank.png');
+    cards[optionIwoId].setAttribute('src', 'images / blank.png');
+    alert('You have clicked the same image!');
+  }
+  if (cardsChosen[0] == cardsChosen[1]) {
+    alert('You found a match!');
+    cards[optionOneId].setAttribute('src', 'images / white.png');
+    cards[optionIwoId].setAttribute('src', 'images / white.png');
+    cards[optionOneId].removeEventListener('click', 'flipCard');
+    cards[optionIwoId].removeEventListener('click', 'flipCard');
+    cardsWon.push(cardsChosen);
+  } else {
+    cards[optionOneId].setAttribute('src', 'images/blank.png');
+    cards[optionIwoId].setAttribute('src', 'images/blank.png');
+    alert('Sorry try again!');
+  }
+  resultDisplay.textContent = cardsWon.length;
+  cardsChosen = [];
+  cardsChosenIds = [];
+
+  if (cardsWon.length == CardArray.length / 2) {
+    resultDisplay.textContent = 'Congratulations you found them all!';
+  }
+}
+
+function flipCard() {
+  const cardId = this.getAttribute('data-id');
+  cardsChosen.push(CardArray[cardId].name);
+  cardsChosenIds.push(cardId);
+  console.log(cardsChosen);
+  console.log(cardsChosenIds);
+  this.setAttribute('src', CardArray[cardId].img);
+  if (cardsChosen.length === 2) {
+    setTimeout(checkMatch, 500);
+  }
+}
